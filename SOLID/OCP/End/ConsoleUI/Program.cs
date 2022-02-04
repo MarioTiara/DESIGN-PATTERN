0bc5ebcbb52de0faa4fff14b2dcd154c6b64a2dd
mario.tiarapratama <mario.tiarapratama@gmail.com>
2022-02-04 10:44:17 +0700

    solution violates OCP

---
+++ SOLID/OCP/End/ConsoleUI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using OCPLibrary;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonModel> applicatns = new List<PersonModel>()
+            {
+                new PersonModel {FirstName ="Mario", LastName="Pratama"},
+                new PersonModel {FirstName="Ahmad", LastName="Fauzan", TypeOfEmployee=EmpoyeeType.Manager},
+                new PersonModel{FirstName="Dery", LastName="Ardiansyah"}
+            };
+
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            Accounts accountsProcessor= new Accounts();
+
+            foreach ( var person in applicatns){
+                employees.Add(accountsProcessor.Create(person));
+            }
+
+            foreach ( var emp in employees){
+                Console.WriteLine ($"{emp.FirstName}{emp.LastName} : {emp.EmailAddress} isManager: {emp.IsManager}");
+            }
+
+        }
+    }
+}
---
+++ SOLID/OCP/End/OCPLibrary/Enum.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace OCPLibrary 
+{
+    public enum EmpoyeeType 
+    {
+        staff,
+        Manager,
+    }
+}
